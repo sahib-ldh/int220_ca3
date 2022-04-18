55a19effd11cadeff0c6177e0a7daff50d8fbd2e
sahib-ldh <85549434+sahib-ldh@users.noreply.github.com>
2022-04-19 00:41:35 +0530

    Add files via upload

---
+++ config/connection.php
@@ -0,0 +1,18 @@
+<?php
+    //local devlopment
+
+    $servername = "localhost";
+    $username = "root";
+     $password = "";
+     $database = "rdb";
+   // remote databse connectionn
+    /*$servername = "remotemysql.com";
+    $username = "gaMS65ybZ0";
+    $password = "KaU41VutZi";
+    $database = "gaMS65ybZ0";*/
+    
+    $conn = new mysqli($servername, $username, $password, $database);
+    if($conn -> connect_error){
+        die("Connection failed: " . $conn -> connect_error);
+    }
+?>
